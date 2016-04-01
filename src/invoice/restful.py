afabffabd07913da35025488c4a5dab006c31a29
Bernard `Guyzmo` Pratz <guyzmo+github@m0g.net>
2016-04-01 20:39:58 +0200

    updated api for react use

--- src/invoice/restful.py
+++ src/invoice/restful.py
@@ -3,33 +3,58 @@
 generate invoices
 
 Usage:
-    restful.py [--verbose] --config=<config> <invoices>
+    restful.py [-v] --config=<config> <invoices>
 
 Parameters:
-    <invoices>          set list of invoices.
-    --config=<config>   Setup config file.
-    --output=<output>   Directory to generate the files to. [default: ./]
-    -v,--verbose        Set verbose output.
-    -h,--help           This message.
-    -V,--version        Show version.
+    <invoices>             set list of invoices.
+    -c,--config=<config>   Setup config file.
+    -o,--output=<output>   Directory to generate the files to. [default: ./]
+    -v,--verbose           Set verbose output.
+    -h,--help              This message.
+    -V,--version           Show version.
 
 """
 
 import os
 
-from flask import Flask, request, make_response, send_from_directory
+from flask import Flask, request, render_template, make_response, send_from_directory
 from flask_restful import Resource, Api
 
+from flask.ext.assets import Bundle, Environment
 from flask.ext.bower import Bower
+
+import jinja2
 
 import invoice
 
+
+def Template(app, path):
+    loader = jinja2.ChoiceLoader([
+        app.jinja_loader,
+        jinja2.FileSystemLoader(path),
+    ])
+    app.jinja_loader = loader
+
+
 def build_api(acct, args=None):
-    app = Flask('pyinvoice')
+    app = Flask('pyinvoice', static_folder='src/static')
     api = Api(app)
 
-    app.config['BOWER_COMPONENTS_ROOT'] = 'static/bower_components'
-    app.config['BOWER_URL_PREFIX'] = '/bower_components'
+    Template(app, 'src/templates')
+
+    app.config["REQUIREJS_BIN"] = 'node_modules/requirejs/bin/r.js'
+    app.config["REQUIREJS_CONFIG"] = 'build.js'
+    app.config["REQUIREJS_RUN_IN_DEBUG"] = False
+
+    if args['--verbose']:
+        app.config["ASSETS_DEBUG"] = True
+
+    assets = Environment(app)
+    assets.set_directory('src/static/scripts/js')
+    assets.set_url('/')
+
+    app.config['BOWER_COMPONENTS_ROOT'] = 'src/static/scripts/js/vendors'
+    app.config['BOWER_URL_PREFIX'] = '/vendors'
 
     Bower(app)
 
@@ -98,28 +123,29 @@ def build_api(acct, args=None):
 
     @app.route('/')
     def basic_pages(**kwargs):
-        return make_response(open(os.path.join(app.root_path,
-                                            'static/app/index.html')).read())
+        return render_template('index.html')
+        #return make_response(open(os.path.join(app.root_path,
+        #                                   'src/static/index.html')).read())
 
-    @app.route('/scripts/<script>')
-    def scripts(script, **kwargs):
-        return send_from_directory('static/app/scripts', script)
+    @app.route('/scripts/<path:script_path>')
+    def scripts(script_path, **kwargs):
+        return send_from_directory('src/static/scripts', script_path)
 
-    @app.route('/scripts/controllers/<script>')
-    def controllers(script, **kwargs):
-        return send_from_directory('static/app/scripts/controllers/', script)
+#     @app.route('/scripts/controllers/<script>')
+#     def controllers(script, **kwargs):
+#         return send_from_directory('src/static/scripts/controllers/', script)
 
-    @app.route('/views/<view>')
-    def views(view, **kwargs):
-        return send_from_directory('static/app/views', view)
+    # @app.route('/views/<view>')
+    # def views(view, **kwargs):
+    #     return send_from_directory('src/static/views', view)
 
     @app.route('/images/<img>')
     def images(img, **kwargs):
-        return send_from_directory('static/app/images', img)
+        return send_from_directory('src/static/images', img)
 
     @app.route('/styles/<style>')
     def styles(style, **kwargs):
-        return send_from_directory('static/app/styles', style)
+        return send_from_directory('src/static/styles', style)
 
 
     app.run(debug=args['--verbose'])
